c15d05b2142fff61142c198e88d4bb9607af96f8
JS0909 <zejace3@gmail.com>
2022-10-15 11:51:21 +0900

    KoGPT2

--- Project/KoGPT2/KoGPT2_generate.py
+++ Project/KoGPT2/KoGPT2_generate.py
@@ -5,29 +5,12 @@ from transformers import AutoModelWithLMHead, PreTrainedTokenizerFast
 from fastai.text.all import *
 import fastai
 import re
+import pickle
 
 tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
   bos_token='</s>', eos_token='</s>', unk_token='<unk>',
   pad_token='<pad>', mask_token='<mask>') 
 model = AutoModelWithLMHead.from_pretrained("skt/kogpt2-base-v2")
-text = """ 옛날 옛날 """
-input_ids = tokenizer.encode(text)
-gen_ids = model.generate(torch.tensor([input_ids]),
-                           max_length=128,
-                           repetition_penalty=2.0,
-                           pad_token_id=tokenizer.pad_token_id,
-                           eos_token_id=tokenizer.eos_token_id,
-                           bos_token_id=tokenizer.bos_token_id,
-                           use_cache=True
-                        )
-generated = tokenizer.decode(gen_ids[0,:].tolist())
-print(generated)
-
-
-
-with open('tale.txt') as f:
-   lines = f.read()
-lines=" ".join(lines.split())
 
 #model input output tokenizer
 class TransformersTokenizer(Transform):
@@ -36,6 +19,15 @@ class TransformersTokenizer(Transform):
        toks = self.tokenizer.tokenize(x)
        return tensor(self.tokenizer.convert_tokens_to_ids(toks))
    def decodes(self, x): return TitledStr(self.tokenizer.decode(x.cpu().numpy()))
+   
+#gpt2 ouput is tuple, we need just one val
+class DropOutput(Callback):
+  def after_pred(self): self.learn.pred = self.pred[0]
+
+'''
+with open('written_test.txt', encoding='UTF8') as f:
+   lines = f.read()
+lines = " ".join(lines.split())
 
 #split data
 train=lines[:int(len(lines)*0.9)]
@@ -44,21 +36,21 @@ splits = [[0],[1]]
 
 #init dataloader
 tls = TfmdLists([train,test], TransformersTokenizer(tokenizer), splits=splits, dl_type=LMDataLoader)
-batch,seq_len = 8,256
+batch, seq_len = 8, 256
 dls = tls.dataloaders(bs=batch, seq_len=seq_len)
 
-#gpt2 ouput is tuple, we need just one val
-class DropOutput(Callback):
-  def after_pred(self): self.learn.pred = self.pred[0]
-      
-      
 learn = Learner(dls, model, loss_func=CrossEntropyLossFlat(), cbs=[DropOutput], metrics=Perplexity()).to_fp16()
 lr=learn.lr_find()
 print(lr)
-learn.fine_tune(6)
+learn.fine_tune(10)
 
+pickle.dump(learn, open(os.path.join('D:\study_data\_data/team_project/korean_written/', 'learn.pkl'), 'wb'))
+'''
 
-prompt=" 옛날 옛날 어느 마을에 흥부와 놀부 형제가 "
+with open(os.path.join('D:\study_data\_data/team_project/korean_written/', 'learn.pkl'), 'rb') as f:
+  learn = pickle.load(f)
+
+prompt=" 강아지 두 마리 "
 prompt_ids = tokenizer.encode(prompt)
 inp = tensor(prompt_ids)[None].cuda()
 preds = learn.model.generate(inp,
@@ -72,3 +64,5 @@ preds = learn.model.generate(inp,
 
 generated = tokenizer.decode(preds[0].cpu().numpy())
 print(generated)
+
+
