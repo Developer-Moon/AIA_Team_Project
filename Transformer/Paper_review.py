b826537fb3b103e51743ba8510797b4cc078a09b
kcw7024 <107664300+kcw7024@users.noreply.github.com>
2022-10-17 10:42:28 +0900

    commit

--- Transformer/Paper_review.py
+++ Transformer/Paper_review.py
@@ -448,6 +448,8 @@ class Transformer(nn.Module):
 
         return output, attention
     
+
+#=====================================================================================================
  
 INPUT_DIM = len(SRC.vocab)
 OUTPUT_DIM = len(TRG.vocab)
@@ -465,6 +467,7 @@ SRC_PAD_IDX = SRC.vocab.stoi[SRC.pad_token]
 TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
 
 # 인코더(encoder)와 디코더(decoder) 객체 선언
+# input_dim, hidden_dim, n_layers, n_heads, pf_dim, dropout_ratio, device, max_length=100
 enc = Encoder(INPUT_DIM, HIDDEN_DIM, ENC_LAYERS, ENC_HEADS, ENC_PF_DIM, ENC_DROPOUT, device)
 dec = Decoder(OUTPUT_DIM, HIDDEN_DIM, DEC_LAYERS, DEC_HEADS, DEC_PF_DIM, DEC_DROPOUT, device)
 
@@ -606,14 +609,131 @@ for epoch in range(N_EPOCHS):
     print(f'\tValidation Loss: {valid_loss:.3f} | Validation PPL: {math.exp(valid_loss):.3f}')
     
 
-# # 학습된 모델 저장
-# from google.colab import files
+# 학습된 모델 저장
+torch.save(model.state_dict(), 'transformer_german_to_english.pt')
 
-# files.download('transformer_german_to_english.pt')
+model.load_state_dict(torch.load('transformer_german_to_english.pt'))
+
+test_loss = evaluate(model, test_iterator, criterion)
+
+print(f'Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):.3f}')
+# PPL : 언어모델의 평가 방법. Perplexity
+
+# 학습된 모델 불러오기
+model = Transformer(enc, dec, SRC_PAD_IDX, TRG_PAD_IDX, device).to(device)
+model.load_state_dict(torch.load('transformer_german_to_english.pt'))
+
+test_loss = evaluate(model, test_iterator, criterion)
+
+print(f'Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):.3f}')
+
+# 번역(translation) 함수
+def translate_sentence(sentence, src_field, trg_field, model, device, max_len=50, logging=True):
+    model.eval() # 평가 모드
+
+    if isinstance(sentence, str):
+        nlp = spacy.load('de')
+        tokens = [token.text.lower() for token in nlp(sentence)]
+    else:
+        tokens = [token.lower() for token in sentence]
+
+    # 처음에 <sos> 토큰, 마지막에 <eos> 토큰 붙이기
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+    if logging:
+        print(f"전체 소스 토큰: {tokens}")
+
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+    if logging:
+        print(f"소스 문장 인덱스: {src_indexes}")
+
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+
+    # 소스 문장에 따른 마스크 생성
+    src_mask = model.make_src_mask(src_tensor)
+
+    # 인코더(endocer)에 소스 문장을 넣어 출력 값 구하기
+    with torch.no_grad():
+        enc_src = model.encoder(src_tensor, src_mask)
+
+    # 처음에는 <sos> 토큰 하나만 가지고 있도록 하기
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+
+    for i in range(max_len): # max_len 만큼 반복해서 단어 뽑기
+        trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+
+        # 출력 문장에 따른 마스크 생성
+        trg_mask = model.make_trg_mask(trg_tensor)
+
+        with torch.no_grad():
+            output, attention = model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+
+        # 출력 문장에서 가장 마지막 단어만 사용
+        pred_token = output.argmax(2)[:,-1].item()
+        trg_indexes.append(pred_token) # 출력 문장에 더하기
+
+        # <eos>를 만나는 순간 끝
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+
+    # 각 출력 단어 인덱스를 실제 단어로 변환
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+
+    # 첫 번째 <sos>는 제외하고 출력 문장 반환
+    return trg_tokens[1:], attention
 
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def display_attention(sentence, translation, attention, n_heads=8, n_rows=4, n_cols=2):
+
+    assert n_rows * n_cols == n_heads
+
+    # 출력할 그림 크기 조절
+    fig = plt.figure(figsize=(15, 25))
+
+    for i in range(n_heads):
+        ax = fig.add_subplot(n_rows, n_cols, i + 1)
+
+        # 어텐션(Attention) 스코어 확률 값을 이용해 그리기
+        _attention = attention.squeeze(0)[i].cpu().detach().numpy()
+
+        cax = ax.matshow(_attention, cmap='bone')
+
+        ax.tick_params(labelsize=12)
+        ax.set_xticklabels([''] + ['<sos>'] + [t.lower() for t in sentence] + ['<eos>'], rotation=45)
+        ax.set_yticklabels([''] + translation)
+
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
     
+    
+example_idx = 10
+# src = vars(test_dataset.examples[example_idx])['src']
+src = tokenize_de('Zwei Hunde spielen im Schnee.')
+# 모델 출력 결과: two dogs play in the snow . <eos>
 
+# trg = vars(test_dataset.examples[example_idx])['trg']
+# trg = tokenize_en('I love you.')
+
+print(f'소스 문장: {src}')
+# print(f'타겟 문장: {trg}')
+
+translation, attention = translate_sentence(src, SRC, TRG, model, device, logging=True)
+
+print("모델 출력 결과:", " ".join(translation))
+
+'''
+src = tokenize_de('Zwei Hunde spielen im Schnee.')
+소스 문장: ['Zwei', 'Hunde', 'spielen', 'im', 'Schnee', '.']
+전체 소스 토큰: ['<sos>', 'zwei', 'hunde', 'spielen', 'im', 'schnee', '.', '<eos>']
+소스 문장 인덱스: [2, 18, 121, 57, 20, 123, 4, 3]
+모델 출력 결과: two dogs play in the snow . <eos>
+
+
+'''
 
     
 
